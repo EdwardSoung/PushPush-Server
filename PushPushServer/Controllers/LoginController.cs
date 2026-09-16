d1c2e3a00d0b3eba0f562eef98000880b5c02d0e
edwardsoung <allegoria88@Gmail.com>
2026-09-17 01:50:16 +0900

    기본 .NET 웹서버 프로젝트 세팅

---
+++ PushPushServer/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PushPushServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        //로그인 userId DB 조회
+        //존재하면 음... 뭔가 유저 고유 키값 필요..
+        //없으면 만들어줌
+        //토큰 만들어서 내려줌
+        //유저 고유 키값으로 나머지 데이터 처리 필요함...
+    }
+}
---
+++ PushPushServer/Controllers/ScoreController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PushPushServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoreController : ControllerBase
+    {
+        [HttpPost(Name ="SetScore")]
+        [HttpPost("SetScore")]
+        public void SetScore(int objects)
+        {
+
+        }
+    }
+}
